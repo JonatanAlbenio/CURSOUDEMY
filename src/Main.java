a0d7f18a7722f01bbed62d8cc62e13d2d3d8b66e
Jonatan medina <jonatan.medina@uniminuto.edu.co>
2022-12-29 17:54:20 -0500

    primero

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String saludar ="hola desde java";
+        System.out.println(saludar);
+        System.out.println("saludar = " + saludar.toUpperCase());
+        int numero =10;
+        System.out.println("numero"+numero);
+        boolean valor = true;
+                if(valor){
+
+                    System.out.println("numero"+numero);
+                    int numero2=12;
+
+                }
+                String nombre;
+                nombre="andres";
+                if (numero>=10){
+                    nombre="jonatan";
+                }
+        System.out.println("nombre"+ nombre);
+    }
+
+}
