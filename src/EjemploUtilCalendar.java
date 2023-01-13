23a9a34278375468743488a25e6214214cdaa262
Jonatan medina <jonatan.medina@uniminuto.edu.co>
2023-01-13 16:53:36 -0500

    16

--- src/EjemploUtilCalendar.java
+++ src/EjemploUtilCalendar.java
@@ -10,7 +10,13 @@ public class EjemploUtilCalendar {
 
 
         calendar.set(2022, calendar.APRIL,3,19,2,3);
+        calendar.set(Calendar.MONTH,Calendar.JULY);
+        calendar.set(Calendar.DAY_OF_MONTH,25);
+
+        calendar.set(Calendar.HOUR_OF_DAY,21);
+
         Date fecha = calendar.getTime();
+
 
         System.out.println("fecha = " + fecha) ;
 
